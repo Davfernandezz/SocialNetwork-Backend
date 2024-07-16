b1213d59164021bdf597ba1b61d15e470d4855d7
David Fernadez <daffevafernandez@gmail.com>
2024-07-16 17:03:45 +0200

    feat: login endpoint created with their respective routes

--- src/entities/users/users.controllers.js
+++ src/entities/users/users.controllers.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import User from "./user.model.js";
+import jwt from "jsonwebtoken";
 
 //POST
 export const register = async (req, res) => {
@@ -21,6 +22,52 @@ export const register = async (req, res) => {
         res.status(500).json({
             success: false,
             message: "Error registering user",
+            error: error.message
+        })
+    }
+}
+
+//POST
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body
+
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "Email and Password are needed"
+            })
+        }
+        const user = await User.findOne({
+            email: email
+        })
+        if (!user) {
+            return res.status(400).json({
+                success: false,
+                message: "Email or password not valid"
+            })
+        }
+        if (!bcrypt.compareSync(password, user.password)) {
+            return res.status(400).json({
+                succes: false,
+                message: "Email or password not valid"
+            })
+        }
+       const token = jwt.sign({
+            id: user.id,
+            role: user.role
+          }, process.env.JWT_SECRET, { expiresIn: '2h' });
+
+        return res.status(200).json({
+            success: true,
+            Message: "User logged",
+            id: user.id,
+            token: token
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Error log in user",
             error: error.message
         })
     }
--- src/entities/users/users.routes.js
+++ src/entities/users/users.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { register } from "./users.controllers.js";
+import { login, register } from "./users.controllers.js";
 
 const router = Router()
 
 router.post('/register', register)
+router.post('/login', login)
 
 export {router}
