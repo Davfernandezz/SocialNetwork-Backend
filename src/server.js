c111102b3f5222a5f856f6e08457424802e0ba6a
David Fernadez <daffevafernandez@gmail.com>
2024-07-16 16:45:51 +0200

    feat: database server connection established

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'dotenv/config'
+import { dbConnection } from './database/db.js';
 
 const app = express();
 
@@ -7,13 +8,20 @@ app.use(express.json())
 
 const PORT = process.env.PORT
 
-app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`)
-})
-
 app.get('/healthy', (req, res) => {
     res.json({
         success: true,
         message: "Server is healthy!"
     });
 });
+
+dbConnection()
+    .then(() => {
+        console.log('Database Connected');
+        app.listen(PORT, () => {
+            console.log(`Server running ${PORT}`);
+        });
+    })
+    .catch(error => {
+        console.log('Error conecction database: ' + error.message);
+    })
