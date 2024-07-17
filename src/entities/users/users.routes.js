d303558decc0bcb1307e10bdc8c1bda1dfd14f69
David Fernadez <daffevafernandez@gmail.com>
2024-07-17 21:46:32 +0200

    feat: isAdmin middleware imported into getAllUsers

--- src/entities/users/users.routes.js
+++ src/entities/users/users.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getAllUsers, getUserProfile, updateUserProfile } from "./users.controllers.js";
 import { auth } from "../../middlewares/auth.js";
+import { isAdmin } from "../../middlewares/isAdmin.js";
 
 const router = Router()
 
-router.get('/',auth , getAllUsers)
+router.get('/',auth ,isAdmin , getAllUsers)
 router.get('/profile',auth , getUserProfile)
 router.put('/profile/:id',auth , updateUserProfile)
 
