ea3a3a6d5a6d2c2494e3cb1a7c7f23790bb27993
David Fernadez <daffevafernandez@gmail.com>
2024-07-17 16:45:17 +0200

    feat: creation of createPost endpoint and route

--- src/entities/posts/posts.controllers.js
+++ src/entities/posts/posts.controllers.js
@@ -1,9 +1,46 @@
+import Post from "./post.model.js";
+import User from "../users/user.model.js"
 
 //POST
 export const createPost = async (req, res) => {
     try {
-       
+        const { description } = req.body
+        const userId = req.tokenData.id
+        if (!description) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid description"
+            })
+        }
+        const user = await User.findOne({
+            _id: userId
+        })
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                message: "User does not exist"
+            })
+        }
+        const newPost = await Post.create(
+            {
+                description: description,
+                userId: userId,
+            }
+        )
+        res.status(200).json(
+            {
+                success: true,
+                message: "Post created",
+                data: newPost,
+            }
+        )
     } catch (error) {
-       
+        res.status(500).json(
+            {
+                success: false,
+                message: 'Error creating post',
+                error: error.message
+            }
+        )
     }
 }
