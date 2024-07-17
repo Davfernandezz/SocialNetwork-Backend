e2f3e6c977ff9d40ce5f953e06b325cb3ffe82c2
David Fernadez <daffevafernandez@gmail.com>
2024-07-17 17:08:20 +0200

    fix: new auth entity and adaptation of routes

--- src/entities/users/users.routes.js
+++ src/entities/users/users.routes.js
@@ -1,11 +1,9 @@
 import { Router } from "express";
-import { getAllUsers, getUserProfile, login, register, updateUserProfile } from "./users.controllers.js";
+import { getAllUsers, getUserProfile, updateUserProfile } from "./users.controllers.js";
 import { auth } from "../../middlewares/auth.js";
 
 const router = Router()
 
-router.post('/register', register)
-router.post('/login', login)
 router.get('/',auth , getAllUsers)
 router.get('/profile',auth , getUserProfile)
 router.put('/profile/:id',auth , updateUserProfile)
